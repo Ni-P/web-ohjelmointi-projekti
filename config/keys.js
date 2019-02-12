3bebda4664f645bbc65d44c8ba815b50f7c36417
Niko Pinnis <niko.pinnis@gmail.com>
2019-02-12 13:12:41 +0200

    Preparing for deployment.

--- config/keys.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    mongoURI: {
-        remote: "mongodb://administrator:c3B6GpQe7hnp86w@ds119445.mlab.com:19445/nip-web-ohjelmointi",
-        local: "mongodb://localhost:27017"
-    },
-    cookieKey: "nsunclksau4fhqow387rhiouncfqo2u43bfoqwuf"
-};
